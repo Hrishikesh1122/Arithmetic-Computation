948615c7099ce68fcecc8e5e2cb16cd6b04ded75
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-26 10:21:27 +0530

    Added ComputeUC3 function

---
+++ ComputeUC3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+function computeUC2()
+{
+ val=$(($1+$2*$3))
+ return $val
+}
+
+function computeUC3()
+{
+ val=$(($1*$2+$3))
+ return $val
+}
+
+
+read -p "Enter value of a " a
+read -p "Enter value of b " b
+read -p "Enter value of c " c
+echo $a $b $c
+computeUC2 $a $b $c
+computeUC3 $a $b $c
+
+
