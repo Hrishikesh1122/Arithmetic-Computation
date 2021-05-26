51fafa94b87ba61c4ab950af0ce7070fcd8d4a80
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-26 10:36:58 +0530

    Added ComputeUC5 function

---
+++ ComputeUC5.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+function computeUC2()
+{
+ val=$(($1+$2*$3))
+ return $val
+}
+
+function computeUC3()
+{
+ val=$(($1*$2+$3))
+ return $val
+}
+
+function computeUC4()
+{
+ val=$(($3+$1/$2))
+ return $val
+}
+
+function computeUC5()
+{
+ val=$(($1%$2+$3))
+ return $val
+}
+
+
+read -p "Enter value of a " a
+read -p "Enter value of b " b
+read -p "Enter value of c " c
+echo $a $b $c
+computeUC2 $a $b $c
+computeUC3 $a $b $c
+computeUC4 $a $b $c
+computeUC5 $a $b $c
+
+
